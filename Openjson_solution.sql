d99cc299acd6e00e0faa3f4dcecf860a81c1352b
Lakshmikanth <k.lakshmikanth002@gmail.com>
2023-09-28 17:22:59 +0530

    Add files via upload

---
+++ Openjson_solution.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{EA9F4364-D501-4AA6-9419-5E81F6016FEC}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {47678E5E-3307-4970-B8C1-8EA485A4E553}
+	EndGlobalSection
+EndGlobal
